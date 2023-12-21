839e9a2469a5ed45ff62d7d8e8dac80acf881e46
Esko Luontola <esko@luontola.fi>
2023-12-21 15:32:57 +0200

    Replace Mocha with Vitest

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,8 +1,9 @@
+import { describe, test } from "vitest";
 import { expect } from "chai";
 import { sum } from "../src/example.mjs";
 
 describe("Example test fixture", () => {
-  it("Example test", () => {
+  test("Example test", () => {
     expect(sum(1, 2)).to.equal(3);
   });
 });
